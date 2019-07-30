44fb58e16e6090a5ef0a4e7b69e2f8f6446db33d
Nicole Dow <nikki.dow71@gmail.com>
2019-07-30 01:38:05 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,10 +2,13 @@ class Dog
   attr_accessor :owner, :mood
   attr_reader :name
   
+  @@all = []
+  
   def initialize(name, owner)
     @name = name
     @owner = owner
     @mood = "nervous"
+    @@all << self
   end
   
 end
